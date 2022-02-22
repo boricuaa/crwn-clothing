7f9210111e46444e1cce141e7f26d847b4aec7d4
Jose <jose@Joses-MacBook-Pro.local>
2022-02-21 20:12:50 -0700

    style our menu item

---
+++ src/components/menu-item/menu-item.jsx
@@ -0,0 +1,19 @@
+import './menu-item-styles.scss';
+const Menuitem = ({title, imageUrl, size}) => (
+    <div className={`${size} menu-item`}>
+        <div className='background-image' style={{
+        // string interpolation javascript in react below with image url
+        backgroundImage: `url(${imageUrl})`
+    }} />
+    <div className="content">
+        <div className="title">
+            <div className="subtitle">
+                <h1 className='title'>{title.toUpperCase()}</h1>
+                <span className="subtitle">Shop Now</span>
+            </div>
+        </div>
+    </div>
+</div>
+);
+
+export default Menuitem;
---
+++ src/pages/homepage.jsx
@@ -0,0 +1,11 @@
+import Directory from '../components/directory/directory';
+import './homepage.styles.scss'
+
+
+const Homepage = () => (
+    <div className="homepage">
+       <Directory /> 
+    </div>
+)
+
+export default Homepage;
